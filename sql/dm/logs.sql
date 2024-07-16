d41296b204b27c32bddfd494fc84c4ac08e378ff
ostdari <115879627+ostdari@users.noreply.github.com>
2024-07-17 02:35:18 +0300

    удаление лишнего

--- sql/dm/logs.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE logs.logs (
-    file_name VARCHAR(50),
-    table_name VARCHAR(50),
-    start_time TIMESTAMP,
-    end_time TIMESTAMP,
-    row_count INTEGER,
-	status varchar(20)
-);
