f2ee3e9d7b3b81d61394bcf199953cc1d11ea219
ostdari <115879627+ostdari@users.noreply.github.com>
2024-07-20 21:12:06 +0300

    Delete sql/ds/DS.FT_POSTING_F.sql

--- sql/ds/DS.FT_POSTING_F.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS DS.FT_POSTING_F (
-	oper_date DATE not null,
-	credit_account_rk integer not null,
-	debet_account_rk integer not null,
-	credit_amount FLOAT,
-	debet_amount FLOAT
-);
